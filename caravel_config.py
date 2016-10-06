7b799fc5cda1b568e0bfcd9c32e1fff386a73a23
Charles Baakel <charles@applepiecapital.com>
2016-10-06 16:05:29 -0700

    update config

--- caravel_config.py
+++ caravel_config.py
@@ -12,7 +12,7 @@ CARAVEL_WORKERS = 1 # for it to work in heroku basic/hobby dynos increase as you
 # Flask App Builder configuration
 #---------------------------------------------------------
 # Your App secret key
-SECRET_KEY = '\2\1thisismyscretkey\1\2\e\y\y\h'  # noqa
+SECRET_KEY = '\2\1thiPzsmyA29etkey\1\2\e\y\y\h'  # noqa
 
 # The SQLAlchemy connection string to your database backend
 # This connection defines the path to the database that stores your
